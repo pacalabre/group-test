67151b1368c618c0bff9776eea55f75f052af57a
Paul <pacalabre@gmail.com>
2016-05-12 14:20:24 -0700

    added some content to the paul page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/emilie'  => 'main#emilie'
   get '/drew' => 'main#drew'
 
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
